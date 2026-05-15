483e3ca9baab4dcc8b4a69471e974fe6b0d271d6
ecom-single11 <ecomgithub@gmail.com>
2026-05-15 14:08:55 +0530

    Update usersRoutes.js

--- API/routes/usersRoutes.js
+++ API/routes/usersRoutes.js
@@ -4,7 +4,7 @@ const usersController = require('../controllers/usersController');
 const { auth } = require('../middlewares/auth.js');
 
 router.post('/createUser',auth, usersController.createUser);
-router.get('/getAllUsers',auth, usersController.getAllUsers);
+router.get('/getAllUsers', usersController.getAllUsers);
 router.get('/getAllUsersByPage',auth, usersController.getAllUsersByPage);
 router.post('/loginUser', usersController.loginUser);
 router.put('/updateUser/:id',auth, usersController.updateUser);
